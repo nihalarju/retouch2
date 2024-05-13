d2960adac80d6872150231dbfca888badd87b094
Nihal <faraday-loss-0i@icloud.com>
2024-05-12 17:21:10 -0700

    cv2 scanner added. not working great to identify contours yet

--- cv2_scanner.py
+++ cv2_scanner.py
@@ -3,18 +3,22 @@
 #Importing Libraries
 import cv2
 import numpy as np
+import glob
+import os
 
 # Loading an image and setting the frame size for the scanned documents
-img = cv2.imread("docs.jpg")
-frameWidth = 480
-frameHeight = 640
-cv2.imshow("Original",img)
+sourcedir = r"/Volumes/termez/DocumentsMac/intel/1280 process front end handbook/0_photographs/"
+targetdir = r"/Volumes/termez/DocumentsMac/intel/1280 process front end handbook/1_scaled/"
+
+frameWidth = 1800
+frameHeight = 1200
+# cv2.imshow("Original",img)
 
 #Processing the image
 def imageProcessing(img):
     imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     imgBlur = cv2.GaussianBlur(imgGray,(5,5),1)
-    imgCanny = cv2.Canny(imgBlur,200,200)
+    imgCanny = cv2.Canny(imgBlur,5,5)
     return imgCanny
 
 
@@ -25,12 +29,13 @@ def GetContour(img):
     contours,heirarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
     for cnt in contours:
         area = cv2.contourArea(cnt)
-        if area > 5500 :
+        if area > 550 :
             peri = cv2.arcLength(cnt,True)
             approx = cv2.approxPolyDP(cnt,0.02 * peri, True)
             if area > bArea and len(approx) == 4:
                 biggest = approx
                 bArea = area
+    assert len(biggest) == 4
     return biggest
 
 #Getting the points around the boundaries
@@ -55,12 +60,39 @@ def getWarp(img,biggest):
     imgOutput = cv2.warpPerspective(img, matrix, (frameWidth, frameHeight))
     return imgOutput
 
-#Using the functions to get the scanned document
-img = cv2.resize(img,(frameWidth,frameHeight))
-result = imageProcessing(img)
-biggest = GetContour(result)
-imgWrapped = getWarp(img, biggest)
-cv2.imshow("Output",imgWrapped)
+
+# img = cv2.imread(r"/Volumes/termez/DocumentsMac/intel/1280 process front end handbook/0_photographs/IMG_7399.jpeg")
+# #Using the functions to get the scanned document
+# img = cv2.resize(img,(frameWidth,frameHeight))
+# result = imageProcessing(img)
+# biggest = GetContour(result)
+# imgWrapped = getWarp(img, biggest)
+# cv2.imshow("Output",imgWrapped)
+# cv2.waitKey(0)
 
 
-cv2.waitKey(0)
+img_paths = glob.glob(sourcedir + "*.jpeg")
+fail_count = 0
+margin = 100
+
+for img_path in img_paths:
+    try:
+        img = cv2.imread(img_path)
+        img = cv2.resize(img,(frameWidth,frameHeight))
+        img = img[margin:-int(3*margin), margin:-3*margin]
+        imgCanny = imageProcessing(img)
+        biggest = GetContour(imgCanny)
+        imgWrapped = getWarp(img, biggest)
+        img_target_path = os.path.join(targetdir, os.path.basename(img_path) )
+        # print(img_path, img_target_path)
+        cv2.imwrite(img_target_path, imgWrapped)
+    except AssertionError:
+        fail_count += 1
+        # print(img_path)
+        # print("biggest: ", biggest)
+        cv2.imshow("Input",imgCanny)
+        cv2.waitKey(0)
+        break 
+        # pass
+    
+print("failed images: ", fail_count)
